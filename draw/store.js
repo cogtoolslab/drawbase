9387a5ea04c20d52ddd29248a00164d2caac2633
Judy Fan <judithfan@gmail.com>
2018-10-03 13:33:58 -0700

    initial attempt to define single vs batch stim fetching call to mongo

--- draw/store.js
+++ draw/store.js
@@ -111,19 +111,19 @@ function serve() {
     });
 
 
-    app.post('/db/getstims', (request, response) => {
+    app.post('/db/getsinglestim', (request, response) => {
       if (!request.body) {
-        return failure(response, '/db/getstims needs post request body');
+        return failure(response, '/db/getsinglestim needs post request body');
       }
       console.log(`got request to get stims from ${request.body.dbname}/${request.body.colname}`);
 
       const databaseName = request.body.dbname;
       const collectionName = request.body.colname;
       if (!collectionName) {
-        return failure(response, '/db/getstims needs collection');
+        return failure(response, '/db/getsinglestim needs collection');
       }
       if (!databaseName) {
-        return failure(response, '/db/getstims needs database');
+        return failure(response, '/db/getsinglestim needs database');
       }
 
       const database = connection.db(databaseName);
@@ -138,12 +138,52 @@ function serve() {
         if(err) {
           console.log(err);
         } else {
-	    // Immediately mark as annotated so others won't get it too
-	    markAnnotation(collection, request.body.gameid, results[0]['_id']);
+    	    // Immediately mark as annotated so others won't get it too
+    	    markAnnotation(collection, request.body.gameid, results[0]['_id']);
           response.send(results[0]);
         }
       });
     });
+
+
+    app.post('/db/getbatchstims', (request, response) => {
+      if (!request.body) {
+        return failure(response, '/db/getbatchstims needs post request body');
+      }
+      console.log(`got request to get stims from ${request.body.dbname}/${request.body.colname}`);
+
+      const databaseName = request.body.dbname;
+      const collectionName = request.body.colname;
+      if (!collectionName) {
+        return failure(response, '/db/getbatchstims needs collection');
+      }
+      if (!databaseName) {
+        return failure(response, '/db/getbatchstims needs database');
+      }
+
+      const database = connection.db(databaseName);
+      const collection = database.collection(collectionName);
+
+      // get all records, up to some crazy number
+      const maxRecords = 500; 
+      let records = collection.find()
+      if(records.count() < maxRecords) {
+        records = records.limit(maxRecords)
+      } 
+      response.send(records.toArray())  
+      // note that the following might work, but I don't know what limit
+      // does if you pass it a number greater than the total number of records
+ 
+      // response.send(collection.limit(maxRecords).toArray())
+
+      // also, note sure if we need to mark...
+
+      // markAnnotation(collection, request.body.gameid, results[0]['_id']);
+    });
+
+
+
+
 
     app.listen(port, () => {
       log(`running at http://localhost:${port}`);
